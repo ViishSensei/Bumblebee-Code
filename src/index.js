581e17c39faea38a73da069f71bda6d75831e5bf
Viish <104511692+ViishSensei@users.noreply.github.com>
2025-01-19 23:23:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,5 @@
-const config = require('./config/config.json');
-const Bumblebee = require('./base/BumblebeeClient.js');
-const { accessories } = require('./structures/clientAccessories.js');
-const { collections } = require('./structures/clientCollection.js');
-const { databases } = require('./structures/clientDatabase.js');
-const { handlers } = require('./structures/clientHandler.js');
-
+import Bumblebee from './base/Client.js';
+import config from './config/config.js';
 const client = new Bumblebee();
-
-client.on("ready", () => {
-  collections(client);
-  accessories(client);
-  databases(client);
-  handlers(client);
-  console.log(`Logged in as ${client.user.tag}`);
-});
-
-client.login(config.token)
-  .catch(error => {
-    console.error('Login failed:', error);
-    process.exit(1);
-  });
-
-module.exports = client;
+client.login(config.token);
+export default client;
